a08873cd1a217c403e5c86f7c90b60bbfd9fc072
kppalmo <kpalmo@ithaca.edu>
2022-02-11 08:55:53 -0500

    admin

--- src/main/java/edu/ithaca/dturnbull/bank/BankAdmin.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAdmin.java
@@ -1,5 +1,17 @@
+package edu.ithaca.dturnbull.bank;
 public class BankAdmin {
-    private int temp;
+
+    //Method will Open a Closed Account
+    public void openAccount(String email){
+        
+        
+
+    }
+
+    //Method will Freeze a Open Account
+    public void freezeAccount(String email){
+
+    }
     
 }
 
