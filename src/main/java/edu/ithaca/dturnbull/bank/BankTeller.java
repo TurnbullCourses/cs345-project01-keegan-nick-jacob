a1722eea2fa78e0f0b43916afb458361b336e1cc
Jacob Reed <jakereed292@gmail.com>
2022-02-13 23:17:29 -0500

    Added Teller open account

---
+++ src/main/java/edu/ithaca/dturnbull/bank/BankTeller.java
@@ -0,0 +1,22 @@
+package edu.ithaca.dturnbull.bank;
+
+public class BankTeller {
+    private BankAccount[] accounts;
+
+    public BankTeller(BankAccount[] accountsIn){
+        accounts = accountsIn;
+    }
+
+    public void openAccount(String accountTypeIn){
+        if(accountTypeIn.equals("Savings")){
+            SavingsAccount savings = new SavingsAccount(0, 0.06);
+        }
+        else if (accountTypeIn.equals("Checking")){
+            CheckingAccount checking = new CheckingAccount();
+        }
+    }
+
+    
+    
+}
+
