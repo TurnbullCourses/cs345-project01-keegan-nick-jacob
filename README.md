8ec07923110c70e865e78f816a185c81f79f52c0
kppalmo <kpalmo@ithaca.edu>
2022-02-28 09:05:38 -0500

    finalReadMeChanges

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Three Use Cases that you will implement first:
 
 Which classes & methods do you need to implement for these use cases?
 (Indicated which team member is responsible for the implemenation)
-1. - Admin class - freeze/unfreeze account / test for all cases - Keegan
+1. - Admin class - freeze/unfreeze account / transfer / isAmountValid / Deposit / test for all cases - Keegan
 2. - Customer / bankAccount rework - Nick
 3. - Teller / Savings / Checkings - Jacob
 4. -
--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -51,5 +51,38 @@ public class BankAccount {
             throw new ClosedAccountException("The Account is Frozen");
         }
     }
+    public static boolean isAmountValid(double amount) {
+        // method takes in a double and checks amount of decimal points
+        if (amount < 0) {
+            return false;
+        }
+        if ((amount * 100) % 1 != 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void Deposit(double amount) throws InsufficientFundsException {
+        // takes double amount and adds it to current balance
+        if (isAmountValid(amount)) {
+            balance += amount;
+        }
+        else {
+            throw new InsufficientFundsException("Amount Not Valid");
+        }
+    }
+
+    public void Transfer(BankAccount targetBank, double amount) throws InsufficientFundsException {
+        // transfers double amount to another bankAccount obj
+        if (isAmountValid(amount)){
+            //withdraw(amount);
+            targetBank.Deposit(amount);
+        }
+        else{
+            throw new InsufficientFundsException("Amount Not Valid");
+        }
+
+    }
 
 }
